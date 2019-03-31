8c8077fdf499d90a76e79f2100c0c51a4df3d275
Austin <HunterKillerDrone154@gmail.com>
2019-03-30 20:41:26 -0400

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -3,6 +3,7 @@ module.exports.run = (bot, message, args, Discord) => {
   .setTitle("Corgi Bots Commands!")
   .setDescription("Hello! Please read the commands below to learn to use me!")
   .addField("help", "Displays this message!")
+  .addField("rng", "Generates a random number between 0 and a number you specified.")
   .setColor("GREEN")
   .setFooter(`Requested by ${message.author.username}`)
   message.channel.send({embed: em})
