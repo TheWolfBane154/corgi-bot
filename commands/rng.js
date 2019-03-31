c5a74995e36d2e7021f8399d5f414d6d09a51368
Austin <HunterKillerDrone154@gmail.com>
2019-03-30 20:38:09 -0400

    Update rng.js

--- commands/rng.js
+++ commands/rng.js
@@ -1,5 +1,5 @@
 module.exports.run = (bot, message, args, Discord) => {
-let args = args.join(" ")
+let arg = args.join(" ")
 let error = new Discord.RichEmbed()
   .setTitle("Corgi Number Generator Error")
   .addField("Error 404:", "Missing Args please do something like \"!rng 100\" to generate a number between 0 and 100")
@@ -10,7 +10,7 @@ let success = new Discord.RichEmbed()
   .addField("Your number is:", Math.floor(math.random()* args))
   .setColor("GREEN")
   
-  if(!args) {
+  if(!arg) {
   message.channel.send({embed: error})
   } else {
   message.channel.send({embed: success})
